37abc98668137d2c38374931bb714272f7fd9dc9
kocen <kocen@ARIF_KIRIK>
2024-11-21 00:10:12 +0300

    Proje dosyası ekle.

---
+++ 221120241014 Arif Kirik Armstrong sayi/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _221120241014_Arif_Kirik_Armstrong_sayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.WriteLine("bir sayı giriniz:");
+            sayi=Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= sayi; i++)
+            {
+                int a = i / 100;
+                int b = (i - a * 100) / 10;
+                int c = (i - a * 100 - b * 10);
+
+                int d = (a * a * a + b * b * b + c * c * c);
+
+                if (i == d)
+                    Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
